d0296b0ccef55770e8a7d33525f6a5672dbf2d84
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2012-10-20 13:57:39 +1000

    Updated documentation

--- doc.go
+++ doc.go
@@ -18,6 +18,42 @@
 */
 
 /*
+Package qrp provides access to the exported methods of an object across a packet-based connection.
+QRP is designed to work with multiple packet-based protocols such as UDP and SCTP. 
+
+Firstly you must create a node using any of the network specific CreateNode? methods:
+ CreateNode
+ CreateNodeUDP
+
+e.g.
+ 	node, err := CreateNodeUDP("udp", ":50060", 512)
+
+Nodes serve procedures. Procedures are installed using the Register function, which takes an object and
+registers the exported methods of that object.
+
+ 	type AddService struct {}
+ 	
+ 	type AddArgs struct {
+		A, B int32
+	}
+	type AddReply struct {
+		Result int32
+	}
+	
+	func (s *AddService) Add(args *AddArgs, reply *AddReply) {
+		reply.Result = args.A + args.B
+	}
+
+	node.Register(AddService{})
+	
+
+For more infomation on how to use QRP, see rpc_test.go. Most of this library follows the same standards in go/net/rpc.
+
+For more information on the design of the protocol, see doc.go.
+*/
+package qrp
+
+/*
 QRP: A simple packet-based RPC protocol
 
 QRP is a simple efficient protocol for short and simple two-message communications between nodes
@@ -35,39 +71,11 @@ the transaction unique.
 The other type of message is a reply, which has a key 'R'. A reply contains 2 keys.
 The first key is 'R', which maps to the return data. The other key is 'I', which is
 a uint32 used to identify the query the reply is responding to. 
-*/
 
-package qrp
+Why QRP? Why not JSON-RPC?
 
-/*
-Package qrp provides access to the exported methods of an object across a packet-based connection.
-QRP is designed to work with multiple packet-based protocols such as UDP and SCTP. UDP will be used
-as an example.
-
-Firstly you must create a node using any of the CreateNode? methods:
- - CreateNode
- - CreateNodeUDP
-
-e.g.
- 	node, err := CreateNodeUDP("udp", ":50060")
-
-Nodes serve procedures. Procedures are installed using the Register function, which takes an object and
-registers the exported methods of that object.
-
- 	type AddService struct {}
-	func (s *AddService) Add(args *AddArgs, reply *AddReply) {
-		reply.Result = args.A + args.B
-	}
-
-
-	type AddArgs struct {
-		A, B int32
-	}
-	type AddReply struct {
-		Result int32
-	}
-
-	node.Register(AddService{}) // Will export the Add method
-
-For more detail, see rpc_test.go. Much of this library follows the same standards in go/net/rpc
+QRP is designed to be really really minimal. JSON-RPC uses the full names of properties (method 
+instead of m etc.). JSON itself is also quite a bulky encoding scheme in comparison to BEncode. 
+Another difference between JSON-RPC and QRP is the absence of an error property. Errors aren't 
+always useful for a node, so why include them?
 */
